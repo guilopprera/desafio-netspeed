334e1cb352bab8666821411dbefc5cb93e0124a1
João Lopes <joaovictorlp96@outlook.com>
2024-04-17 14:49:54 -0300

    Upload of the project

---
+++ WebApp_Desafio_FrontEnd/Controllers/DepartamentosController.cs
@@ -0,0 +1,174 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using WebApp_Desafio_FrontEnd.ApiClients.Desafio_API;
+using WebApp_Desafio_FrontEnd.ViewModels;
+using WebApp_Desafio_FrontEnd.ViewModels.Enums;
+
+namespace WebApp_Desafio_FrontEnd.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private readonly IHostingEnvironment _hostEnvironment;
+
+        public DepartamentosController(IHostingEnvironment hostEnvironment)
+        {
+            _hostEnvironment = hostEnvironment;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(Listar));
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            // Busca de dados está na Action Datatable()
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Datatable()
+        {
+            try
+            {
+                var departamentosApiClient = new DepartamentosApiClient();
+                var lstDepartamentos = departamentosApiClient.DepartamentosListar();
+
+                var dataTableVM = new DataTableAjaxViewModel()
+                {
+                    length = lstDepartamentos.Count,
+                    data = lstDepartamentos
+                };
+
+                return Ok(dataTableVM);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseViewModel(ex));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            var departamentoVM = new DepartamentoViewModel();
+            ViewData["Title"] = "Cadastrar Novo Departamento";
+
+            try
+            {
+                var departamentosApiClient = new DepartamentosApiClient();
+
+                ViewData["ListaDepartamentos"] = departamentosApiClient.DepartamentosListar();
+            }
+            catch (Exception ex)
+            {
+                ViewData["Error"] = ex.Message;
+            }
+
+            return View("Cadastrar", departamentoVM);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(DepartamentoViewModel departamentoVM)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    // Se o modelo de exibição não for válido, retorne a View com os erros de validação
+                    return View(departamentoVM);
+                }
+
+                var departamentosApiClient = new DepartamentosApiClient();
+                var realizadoComSucesso = departamentosApiClient.DepartamentoGravar(departamentoVM);
+
+                if (realizadoComSucesso)
+                    return Ok(new ResponseViewModel(
+                                $"Departamento gravado com sucesso!",
+                                AlertTypes.success,
+                                this.RouteData.Values["controller"].ToString(),
+                                nameof(this.Listar)));
+                else
+                    throw new ApplicationException($"Falha ao cadastrar o Departamento.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseViewModel(ex));
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult Editar([FromRoute] int id)
+        {
+            ViewData["Title"] = "Cadastrar Novo Departamento";
+
+            try
+            {
+                var departamentosApiClient = new DepartamentosApiClient();
+                var departamentoVM = departamentosApiClient.DepartamentoObter(id);
+
+                ViewData["ListaDepartamentos"] = departamentosApiClient.DepartamentosListar();
+
+                return View("Cadastrar", departamentoVM);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseViewModel(ex));
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Excluir([FromRoute] int id)
+        {
+            try
+            {
+                var departamentosApiClient = new DepartamentosApiClient();
+                var realizadoComSucesso = departamentosApiClient.DepartamentoExcluir(id);
+
+                if (realizadoComSucesso)
+                    return Ok(new ResponseViewModel(
+                                $"Departamento {id} excluído com sucesso!",
+                                AlertTypes.success,
+                                "Departamentos",
+                                nameof(Listar)));
+                else
+                    throw new ApplicationException($"Falha ao excluir o Departamento {id}.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseViewModel(ex));
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Report()
+        {
+            string mimeType = string.Empty;
+            int extension = 1;
+            string contentRootPath = _hostEnvironment.ContentRootPath;
+            string path = Path.Combine(contentRootPath, "wwwroot", "reports", "rptDepartamentos.rdlc");
+            //
+            // ... parameters
+            //
+            LocalReport localReport = new LocalReport(path);
+
+            // Carrega os dados que serão apresentados no relatório
+            var departamentosApiClient = new DepartamentosApiClient();
+            var lstDepartamentos = departamentosApiClient.DepartamentosListar();
+
+            localReport.AddDataSource("dsDepartamentos", lstDepartamentos);
+
+            // Renderiza o relatório em PDF
+            ReportResult reportResult = localReport.Execute(RenderType.Pdf);
+
+            //return File(reportResult.MainStream, "application/pdf");
+            return File(reportResult.MainStream, "application/octet-stream", "rptDepartamentos.pdf");
+        }
+    }
+}
---
+++ WebApp_Desafio_FrontEnd/ViewModels/DepartamentoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+
+namespace WebApp_Desafio_FrontEnd.ViewModels
+{
+    [DataContract]
+    public class DepartamentoViewModel
+    {
+        [DataMember(Name = "ID")]
+        public int ID { get; set; }
+
+        [DataMember(Name = "Descricao")]
+        [MaxLength(50, ErrorMessage = "O campo Descrição deve ter no máximo 50 caracteres.")]
+        public string Descricao { get; set; }
+
+    }
+}
